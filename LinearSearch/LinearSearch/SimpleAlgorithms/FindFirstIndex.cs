5ac28447cbee65d04edcbcec8289af73ddbaeade
nikitaberezhko <berezhko.nik16@gmail.com>
2025-08-18 13:55:10 +0300

    add implementation

--- LinearSearch/LinearSearch/SimpleAlgorithms/FindFirstIndex.cs
+++ LinearSearch/LinearSearch/SimpleAlgorithms/FindFirstIndex.cs
@@ -11,4 +11,16 @@ public partial class SimpleAlgorithms
 
         return null;
     }
+    
+    public static int FindLastIndex(int[] array, int target)
+    {
+        if (array is null) throw new ArgumentNullException(nameof(array));
+        
+        var index = 0;
+        
+        for (var i = 0; i < array.Length; i++)
+            if (array[i] == target) index = i;
+        
+        return index;
+    }
 }
